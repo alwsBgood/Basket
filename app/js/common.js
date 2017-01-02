466b511b212f4a6802fa95f91c284ec54aacdcaa
Дмитрий Чеботару < >
2017-01-03 01:45:49 +0200

    threePages

--- app/js/common.js
+++ app/js/common.js
@@ -5,17 +5,27 @@ $(function() {
 
     var error;
     var btn = $(this);
-    var ref = btn.closest('form').find('[required]');
-    var msg = btn.closest('form').find('input, textarea, select');
-    var send_btn = btn.closest('form').find('[name=send]');
-    var send_options = btn.closest('form').find('[name=campaign_token]');
+    var ref = ('[required]');
+    var msg = ('input, textarea, select');
+    var send_btn = ('[name=send]');
+    var send_options = ('[name=campaign_token]');
+
+    // Cose button isn't inside <form> I had to change vars definition
+    // original is commented
+
+    // var error;
+    // var btn = $(this);
+    // var ref = btn.closest('form').find('[required]');
+    // var msg = btn.closest('form').find('input, textarea, select');
+    // var send_btn = btn.closest('form').find('[name=send]');
+    // var send_options = btn.closest('form').find('[name=campaign_token]');
 
 
 
     $(ref).each(function() {
       if ($(this).val() == '') {
         var errorfield = $(this);
-        $(this).addClass('error').parent('.field').append('<div class="allert"><span>Заполните это поле</span><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 286.1 286.1"><path d="M143 0C64 0 0 64 0 143c0 79 64 143 143 143 79 0 143-64 143-143C286.1 64 222 0 143 0zM143 259.2c-64.2 0-116.2-52-116.2-116.2S78.8 26.8 143 26.8s116.2 52 116.2 116.2S207.2 259.2 143 259.2zM143 62.7c-10.2 0-18 5.3-18 14v79.2c0 8.6 7.8 14 18 14 10 0 18-5.6 18-14V76.7C161 68.3 153 62.7 143 62.7zM143 187.7c-9.8 0-17.9 8-17.9 17.9 0 9.8 8 17.8 17.9 17.8s17.8-8 17.8-17.8C160.9 195.7 152.9 187.7 143 187.7z" fill="#E2574C"/></svg></div>');
+        $(this).addClass('error').parent('.field').append('<div class="allert"><span>Füllen Sie das Feld</span><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 286.1 286.1"><path d="M143 0C64 0 0 64 0 143c0 79 64 143 143 143 79 0 143-64 143-143C286.1 64 222 0 143 0zM143 259.2c-64.2 0-116.2-52-116.2-116.2S78.8 26.8 143 26.8s116.2 52 116.2 116.2S207.2 259.2 143 259.2zM143 62.7c-10.2 0-18 5.3-18 14v79.2c0 8.6 7.8 14 18 14 10 0 18-5.6 18-14V76.7C161 68.3 153 62.7 143 62.7zM143 187.7c-9.8 0-17.9 8-17.9 17.9 0 9.8 8 17.8 17.9 17.8s17.8-8 17.8-17.8C160.9 195.7 152.9 187.7 143 187.7z" fill="#E2574C"/></svg></div>');
         error = 1;
         $(":input.error:first").focus();
         return;
@@ -24,7 +34,7 @@ $(function() {
         if ($(this).attr("type") == 'email') {
           if(!pattern.test($(this).val())) {
             $("[name=email]").val('');
-            $(this).addClass('error').parent('.field').append('<div class="allert"><span>Укажите коректный e-mail</span><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 286.1 286.1"><path d="M143 0C64 0 0 64 0 143c0 79 64 143 143 143 79 0 143-64 143-143C286.1 64 222 0 143 0zM143 259.2c-64.2 0-116.2-52-116.2-116.2S78.8 26.8 143 26.8s116.2 52 116.2 116.2S207.2 259.2 143 259.2zM143 62.7c-10.2 0-18 5.3-18 14v79.2c0 8.6 7.8 14 18 14 10 0 18-5.6 18-14V76.7C161 68.3 153 62.7 143 62.7zM143 187.7c-9.8 0-17.9 8-17.9 17.9 0 9.8 8 17.8 17.9 17.8s17.8-8 17.8-17.8C160.9 195.7 152.9 187.7 143 187.7z" fill="#E2574C"/></svg></div>');
+            $(this).addClass('error').parent('.field').append('<div class="allert"><span>Bitte geben Sie eine gültige E-Mail</span><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 286.1 286.1"><path d="M143 0C64 0 0 64 0 143c0 79 64 143 143 143 79 0 143-64 143-143C286.1 64 222 0 143 0zM143 259.2c-64.2 0-116.2-52-116.2-116.2S78.8 26.8 143 26.8s116.2 52 116.2 116.2S207.2 259.2 143 259.2zM143 62.7c-10.2 0-18 5.3-18 14v79.2c0 8.6 7.8 14 18 14 10 0 18-5.6 18-14V76.7C161 68.3 153 62.7 143 62.7zM143 187.7c-9.8 0-17.9 8-17.9 17.9 0 9.8 8 17.8 17.9 17.8s17.8-8 17.8-17.8C160.9 195.7 152.9 187.7 143 187.7z" fill="#E2574C"/></svg></div>');
             error = 1;
             $(":input.error:first").focus();
           }
@@ -33,7 +43,16 @@ $(function() {
         if ( $(this).attr("type") == 'tel') {
           if(!patterntel.test($(this).val())) {
             $("[name=phone]").val('');
-            $(this).addClass('error').parent('.field').append('<div class="allert"><span>Укажите номер телефона в формате +3809999999</span><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 286.1 286.1"><path d="M143 0C64 0 0 64 0 143c0 79 64 143 143 143 79 0 143-64 143-143C286.1 64 222 0 143 0zM143 259.2c-64.2 0-116.2-52-116.2-116.2S78.8 26.8 143 26.8s116.2 52 116.2 116.2S207.2 259.2 143 259.2zM143 62.7c-10.2 0-18 5.3-18 14v79.2c0 8.6 7.8 14 18 14 10 0 18-5.6 18-14V76.7C161 68.3 153 62.7 143 62.7zM143 187.7c-9.8 0-17.9 8-17.9 17.9 0 9.8 8 17.8 17.9 17.8s17.8-8 17.8-17.8C160.9 195.7 152.9 187.7 143 187.7z" fill="#E2574C"/></svg></div>');
+            $(this).addClass('error').parent('.field').append('<div class="allert"><span>Bitte geben Sie eine gültige Telefonnummer</span><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 286.1 286.1"><path d="M143 0C64 0 0 64 0 143c0 79 64 143 143 143 79 0 143-64 143-143C286.1 64 222 0 143 0zM143 259.2c-64.2 0-116.2-52-116.2-116.2S78.8 26.8 143 26.8s116.2 52 116.2 116.2S207.2 259.2 143 259.2zM143 62.7c-10.2 0-18 5.3-18 14v79.2c0 8.6 7.8 14 18 14 10 0 18-5.6 18-14V76.7C161 68.3 153 62.7 143 62.7zM143 187.7c-9.8 0-17.9 8-17.9 17.9 0 9.8 8 17.8 17.9 17.8s17.8-8 17.8-17.8C160.9 195.7 152.9 187.7 143 187.7z" fill="#E2574C"/></svg></div>');
+            error = 1;
+            $(":input.error:first").focus();
+          }
+        }
+        var patternplz = /^()[0-9]{5}/i;
+        if ( $(this).attr("name") == 'plz') {
+          if(!patterntel.test($(this).val())) {
+            $("[name=plz]").val('');
+            $(this).addClass('error').parent('.field').append('<div class="allert"><span>Geben Sie fünf Zahlen PLZ</span><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 286.1 286.1"><path d="M143 0C64 0 0 64 0 143c0 79 64 143 143 143 79 0 143-64 143-143C286.1 64 222 0 143 0zM143 259.2c-64.2 0-116.2-52-116.2-116.2S78.8 26.8 143 26.8s116.2 52 116.2 116.2S207.2 259.2 143 259.2zM143 62.7c-10.2 0-18 5.3-18 14v79.2c0 8.6 7.8 14 18 14 10 0 18-5.6 18-14V76.7C161 68.3 153 62.7 143 62.7zM143 187.7c-9.8 0-17.9 8-17.9 17.9 0 9.8 8 17.8 17.9 17.8s17.8-8 17.8-17.8C160.9 195.7 152.9 187.7 143 187.7z" fill="#E2574C"/></svg></div>');
             error = 1;
             $(":input.error:first").focus();
           }
@@ -44,13 +63,35 @@ $(function() {
       $(send_btn).each(function() {
         $(this).attr('disabled', true);
       });
-      $(send_options).each(function() {});
+      $(send_options).each(function() {
+
+        // $.ajax({
+        //   type: 'POST',
+        //   url: 'mail.php',
+        //   data: msg,
+        //   success: function() {
+        //     $('form').trigger("reset");
+        //     setTimeout(function(){  $("[name=send]").removeAttr("disabled"); }, 1000);
+        //       // Настройки после удачной отправки
+        //       $('form').trigger("reset");
+        //     },
+        //     error: function(xhr, str) {
+        //       alert('Возникла ошибка: ' + xhr.responseCode);
+        //     }
+        //   });
+
+      });
     }
     return false;
   })
 });
 
 
+// Form trigger
+
+$(".first_form_trigger").click(function() {
+  $('#first_step_form').submit();
+});
 
 
 // Perfect Pxel
