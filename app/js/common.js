728d36978d460a95f98555cd3d3d4548b4dc4344
Дмитрий Чеботару < >
2017-01-09 21:39:46 +0200

    EditsPackFive

--- app/js/common.js
+++ app/js/common.js
@@ -1,3 +1,4 @@
+
 $(function() {
   $("[name=send]").click(function () {
     $(":input.error").removeClass('error');
@@ -5,22 +6,10 @@ $(function() {
 
     var error;
     var btn = $(this);
-    var ref = ('[required]');
-    var msg = ('input, textarea, select');
+    var ref = ('[required=required]');
+    var msg = $('input, textarea, select');
     var send_btn = ('[name=send]');
-    var send_options = ('[name=campaign_token]');
-
-    // Cose button isn't inside <form> I had to change vars definition
-    // original is commented
-
-    // var error;
-    // var btn = $(this);
-    // var ref = btn.closest('form').find('[required]');
-    // var msg = btn.closest('form').find('input, textarea, select');
-    // var send_btn = btn.closest('form').find('[name=send]');
-    // var send_options = btn.closest('form').find('[name=campaign_token]');
-
-
+    var send_options = ('[name=step_count]');
 
     $(ref).each(function() {
       if ($(this).val() == '') {
@@ -50,7 +39,7 @@ $(function() {
         }
         var patternplz = /^()[0-9]{5}/i;
         if ( $(this).attr("name") == 'plz') {
-          if(!patterntel.test($(this).val())) {
+          if(!patternplz.test($(this).val())) {
             $("[name=plz]").val('');
             $(this).addClass('error').parent('.field').append('<div class="allert"><span>Geben Sie fünf Zahlen PLZ</span><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 286.1 286.1"><path d="M143 0C64 0 0 64 0 143c0 79 64 143 143 143 79 0 143-64 143-143C286.1 64 222 0 143 0zM143 259.2c-64.2 0-116.2-52-116.2-116.2S78.8 26.8 143 26.8s116.2 52 116.2 116.2S207.2 259.2 143 259.2zM143 62.7c-10.2 0-18 5.3-18 14v79.2c0 8.6 7.8 14 18 14 10 0 18-5.6 18-14V76.7C161 68.3 153 62.7 143 62.7zM143 187.7c-9.8 0-17.9 8-17.9 17.9 0 9.8 8 17.8 17.9 17.8s17.8-8 17.8-17.8C160.9 195.7 152.9 187.7 143 187.7z" fill="#E2574C"/></svg></div>');
             error = 1;
@@ -64,22 +53,39 @@ $(function() {
         $(this).attr('disabled', true);
       });
       $(send_options).each(function() {
-
-        // $.ajax({
-        //   type: 'POST',
-        //   url: 'mail.php',
-        //   data: msg,
-        //   success: function() {
-        //     $('form').trigger("reset");
-        //     setTimeout(function(){  $("[name=send]").removeAttr("disabled"); }, 1000);
-        //       // Настройки после удачной отправки
-        //       $('form').trigger("reset");
-        //     },
-        //     error: function(xhr, str) {
-        //       alert('Возникла ошибка: ' + xhr.responseCode);
-        //     }
-        //   });
-
+        var form = $(this).closest('form');
+        if ($(this).val() == '1') {
+          // Открываем скрытые поля
+          $('.hidden_form_inputs').slideDown();
+          // Добавляем валидацию скрытых полей
+          $(form).find('input[required]').each(function() {
+            $(this).attr('required', 'required');;
+          })
+          // Меняем отображение текущего шага
+          $('.current_step').addClass('done_step');
+          $('.inactive_step').addClass('current_step').removeClass('inactive_step');
+          // Активируем кнопку
+          setTimeout(function(){  $("[name=send]").removeAttr("disabled"); }, 1000);
+          // Инкрементим шаг отправки чтоб перейти к следующему блоку
+          $(send_options).val(2);
+        } else {
+          $.ajax({
+            type: 'POST',
+            // отправляем данные на нужный адресс
+            url: 'http://www.moebel24.de/CheckoutCombo.do',
+            data: msg,
+            success: function() {
+              setTimeout(function(){  $("[name=send]").removeAttr("disabled"); }, 1000);
+                // Настройки после удачной отправки
+                $('form').trigger("reset");
+                 window.location = ""; // url куда редиректить после отправки сюда
+               },
+               error: function(xhr, str) {
+                console.log('erorr' + xhr.responseCode);
+                alert('Возникла ошибка: ' + xhr.responseCode);
+              }
+            });
+        }
       });
     }
     return false;
@@ -92,6 +98,14 @@ $(function() {
 $(".first_form_trigger").click(function() {
   $('#first_step_form').submit();
 });
+
+// Checkbox
+
+$('[type=checkbox]').click(function() {
+  $(this).toggleClass('input_checked');
+  $(this).hasClass( "input_checked" ) ? $(this).val(1) : $(this).val(0)
+});
+
 
 // Stars
 switch($('.stars').data('stars')) {
